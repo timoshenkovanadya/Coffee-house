ba8a9ba9c5a2ec99951955a121dde7df2fd66db9
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2023-12-05 21:40:53 +0300

    feat: add burger-button

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -3,3 +3,13 @@ const clickHandler = () => {
 };
 
 window.document.querySelector("#button-left").addEventListener("click", clickHandler);
+
+
+window.document.querySelector(".button-icon-burger").addEventListener('click', function() {
+  var element = window.document.querySelector(".new-menu");
+  if (element.classList.contains("new-menu2")) {
+    element.classList.remove("new-menu2");
+  } else {
+    element.classList.add("new-menu2");
+  }
+});
