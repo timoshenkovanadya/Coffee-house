0c99dcc1bc77d03fb58a996fd97568b5b5939844
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2023-12-06 14:45:33 +0300

    feat: start slider

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,15 +1,32 @@
-const clickHandler = () => {
-  window.document.querySelector(".slider").classList.add("test-red");
-};
+window.document
+  .querySelector(".button-icon-burger")
+  .addEventListener("click", function () {
+    var element = window.document.querySelector(".new-menu");
+    if (element.classList.contains("new-menu2")) {
+      element.classList.remove("new-menu2");
+    } else {
+      element.classList.add("new-menu2");
+    }
+  });
 
-window.document.querySelector("#button-left").addEventListener("click", clickHandler);
+let offset = 0;
+const sliderLine = window.document.querySelector(".slider-line");
+window.document
+  .querySelector(".button-right")
+  .addEventListener("click", function () {
+    offset += 1152;
+    if (offset > 2304) {
+      offset=0;
+    }
+    sliderLine.style.left = -offset + "px";
+  });
 
-
-window.document.querySelector(".button-icon-burger").addEventListener('click', function() {
-  var element = window.document.querySelector(".new-menu");
-  if (element.classList.contains("new-menu2")) {
-    element.classList.remove("new-menu2");
-  } else {
-    element.classList.add("new-menu2");
-  }
-});
+  window.document
+  .querySelector(".button-left")
+  .addEventListener("click", function () {
+    offset -= 1152;
+    if (offset < 0) {
+      offset=2304;
+    }
+    sliderLine.style.left = -offset + "px";
+  });
