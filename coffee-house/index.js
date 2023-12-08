d26a33df70b4c0b57d6ed43bdd9770c989412d5b
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2023-12-08 17:43:09 +0300

    feat: add tea and desserts

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -9,110 +9,21 @@ window.document
     }
   });
 
-
-
-
-/*
 let slideIndex = 0;
 const slides = window.document.querySelectorAll(".slide-1, .slide-2, .slide-3");
-const lines = document.querySelectorAll('.line1, .line2');
-const buttonRight = document.querySelector('.button-right');
-const buttonLeft = document.querySelector('.button-left');
-
-function showSlide(n) {
-  // Сбросить стиль всех прогресс-баров
-  lines.forEach((line) => {
-    line.style.background = 'var(--border-light, #c1b6ad)';
-  });
-
-  // Скрыть все слайды
-  slides.forEach((slide, index) => {
-    slide.style.display = 'none';
-  });
-
-  // Показать текущий слайд
-  slides[n].style.display = 'block';
-  fillLine(n);
-}
-
-function fillLine(n) {
-  let width = 0;
-  const id = setInterval(frame, 50); // 50 мс * 100 = 5000 мс = 5 секунд
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      lines[n].style.background = 'var(--border-dark, #665f55)';
-    } else {
-      width++;
-      lines[n].style.background = `linear-gradient(to right, var(--border-dark, #665f55) ${width}%, var(--border-light, #c1b6ad) ${width}%)`;
-    }
-  }
-}
-
-function changeSlide() {
-  showSlide(slideIndex);
-  slideIndex++;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  }
-  }
-  
-buttonRight.addEventListener('click', function() {
-  slideIndex++;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  }
-  showSlide(slideIndex);
-});
-
-buttonLeft.addEventListener('click', function() {
-  slideIndex--;
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
-  showSlide(slideIndex);
-});
-
-changeSlide(); // Показать первый слайд
-setInterval(changeSlide, 5000); // Изменить слайд каждые 5 секунд*/
-
-let slideIndex = 0;
-const slides = window.document.querySelectorAll(".slide-1, .slide-2, .slide-3");
-const lines = document.querySelectorAll('.line1, .line2');
-const buttonRight = document.querySelector('.button-right');
-const buttonLeft = document.querySelector('.button-left');
+const lines = document.querySelectorAll(".line1, .line2, line3");
+const buttonRight = document.querySelector(".button-right");
+const buttonLeft = document.querySelector(".button-left");
 let timerId;
 let progressId;
 let width = 0;
 
 function showSlide(n) {
-  // Сбросить стиль всех прогресс-баров
-  lines.forEach((line) => {
-    line.style.background = 'var(--border-light, #c1b6ad)';
-  });
-
-  // Скрыть все слайды
   slides.forEach((slide, index) => {
-    slide.style.display = 'none';
+    slide.style.display = "none";
   });
 
-  // Показать текущий слайд
-  slides[n].style.display = 'block';
-  width = 0; // Сбросить ширину прогресс-бара
-  fillLine(n);
-}
-
-function fillLine(n) {
-  function frame() {
-    if (width >= 100) {
-      lines[n].style.background = 'var(--border-dark, #665f55)';
-    } else {
-      width++;
-      lines[n].style.background = `linear-gradient(to right, var(--border-dark, #665f55) ${width}%, var(--border-light, #c1b6ad) ${width}%)`;
-      progressId = setTimeout(frame, 50); // 50 мс * 100 = 5000 мс = 5 секунд
-    }
-  }
-  frame();
+  slides[n].style.display = "block";
 }
 
 function changeSlide() {
@@ -124,23 +35,21 @@ function changeSlide() {
 }
 
 function startTimer() {
-  timerId = setInterval(changeSlide, 5000); // Изменить слайд каждые 5 секунд
-  fillLine(slideIndex); // Возобновить прогресс-бар
+  timerId = setInterval(changeSlide, 5000);
 }
 
 function stopTimer() {
   clearInterval(timerId);
-  clearTimeout(progressId); // Остановить прогресс-бар
 }
 
 slides.forEach((slide) => {
-  slide.addEventListener('mouseenter', stopTimer);
-  slide.addEventListener('mouseleave', startTimer);
-  slide.addEventListener('mousedown', stopTimer);
-  slide.addEventListener('mouseup', startTimer);
+  slide.addEventListener("mouseenter", stopTimer);
+  slide.addEventListener("mouseleave", startTimer);
+  slide.addEventListener("mousedown", stopTimer);
+  slide.addEventListener("mouseup", startTimer);
 });
 
-buttonRight.addEventListener('click', function() {
+buttonRight.addEventListener("click", function () {
   slideIndex++;
   if (slideIndex >= slides.length) {
     slideIndex = 0;
@@ -148,7 +57,7 @@ buttonRight.addEventListener('click', function() {
   showSlide(slideIndex);
 });
 
-buttonLeft.addEventListener('click', function() {
+buttonLeft.addEventListener("click", function () {
   slideIndex--;
   if (slideIndex < 0) {
     slideIndex = slides.length - 1;
@@ -156,41 +65,39 @@ buttonLeft.addEventListener('click', function() {
   showSlide(slideIndex);
 });
 
-changeSlide(); // Показать первый слайд
-startTimer(); // Начать таймер
+changeSlide();
+startTimer();
 let touchStartX = 0;
 let touchEndX = 0;
 
 function handleTouchStart(e) {
-    touchStartX = e.changedTouches[0].screenX;
+  touchStartX = e.changedTouches[0].screenX;
 }
 
 function handleTouchMove(e) {
-    touchEndX = e.changedTouches[0].screenX;
+  touchEndX = e.changedTouches[0].screenX;
 }
 
 function handleTouchEnd() {
-    if (touchEndX < touchStartX) {
-        slideIndex++;
-        if (slideIndex >= slides.length) {
-            slideIndex = 0;
-        }
+  if (touchEndX < touchStartX) {
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
     }
+  }
 
-    if (touchEndX > touchStartX) {
-        slideIndex--;
-        if (slideIndex < 0) {
-            slideIndex = slides.length - 1;
-        }
+  if (touchEndX > touchStartX) {
+    slideIndex--;
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
     }
+  }
 
-    showSlide(slideIndex);
+  showSlide(slideIndex);
 }
 
 slides.forEach((slide) => {
-    slide.addEventListener('touchstart', handleTouchStart);
-    slide.addEventListener('touchmove', handleTouchMove);
-    slide.addEventListener('touchend', handleTouchEnd);
+  slide.addEventListener("touchstart", handleTouchStart);
+  slide.addEventListener("touchmove", handleTouchMove);
+  slide.addEventListener("touchend", handleTouchEnd);
 });
-
-
---
+++ coffee-house/menu/index.js
@@ -0,0 +1,107 @@
+const buttoncoffee = window.document.querySelector(".tab-item1");
+const buttontea = window.document.querySelector(".tab-item2");
+const buttondesserts = window.document.querySelector(".tab-item3");
+
+const coffee = window.document.querySelector(".coffee");
+const tea = window.document.querySelector(".tea");
+const desserts = window.document.querySelector(".desserts");
+
+coffee.style.display = "flex";
+tea.style.display = "none";
+desserts.style.display = "none";
+
+buttontea.addEventListener("click", function () {
+  coffee.style.display = "none";
+  tea.style.display = "flex";
+  desserts.style.display = "none";
+});
+
+buttondesserts.addEventListener("click", function () {
+  coffee.style.display = "none";
+  tea.style.display = "none";
+  desserts.style.display = "flex";
+});
+
+buttoncoffee.addEventListener("click", function () {
+  coffee.style.display = "flex";
+  tea.style.display = "none";
+  desserts.style.display = "none";
+});
+
+buttontea.addEventListener("click", function () {
+  buttontea.style.backgroundColor = "#665f55";
+  buttontea.style.color = "#e1d4c9";
+  window.document.querySelector(".icon2").style.backgroundColor = "#e1d4c9";
+  buttoncoffee.style.backgroundColor = "#E1D4C9";
+  buttoncoffee.style.color = "#403F3D";
+  window.document.querySelector(".icon1").style.backgroundColor = "#c1b6ad";
+  buttondesserts.style.backgroundColor = "#E1D4C9";
+  buttondesserts.style.color = "#403F3D";
+  window.document.querySelector(".icon3").style.backgroundColor = "#c1b6ad";
+});
+buttondesserts.addEventListener("click", function () {
+  buttondesserts.style.backgroundColor = "#665f55";
+  buttondesserts.style.color = "#e1d4c9";
+  window.document.querySelector(".icon3").style.backgroundColor = "#e1d4c9";
+  buttoncoffee.style.backgroundColor = "#E1D4C9";
+  buttoncoffee.style.color = "#403F3D";
+  window.document.querySelector(".icon1").style.backgroundColor = "#c1b6ad";
+  buttontea.style.backgroundColor = "#E1D4C9";
+  buttontea.style.color = "#403F3D";
+  window.document.querySelector(".icon2").style.backgroundColor = "#c1b6ad";
+});
+buttoncoffee.addEventListener("click", function () {
+  buttoncoffee.style.backgroundColor = "#665f55";
+  buttoncoffee.style.color = "#e1d4c9";
+  window.document.querySelector(".icon1").style.backgroundColor = "#e1d4c9";
+  buttondesserts.style.backgroundColor = "#E1D4C9";
+  buttondesserts.style.color = "#403F3D";
+  window.document.querySelector(".icon3").style.backgroundColor = "#c1b6ad";
+  buttontea.style.backgroundColor = "#E1D4C9";
+  buttontea.style.color = "#403F3D";
+  window.document.querySelector(".icon2").style.backgroundColor = "#c1b6ad";
+});
+
+const buttonrefresh = window.document.querySelector(".button-refresh");
+function initRefresh() {
+  if (
+    (window.innerWidth <= 768 && coffee.style.display === "flex") ||
+    (window.innerWidth <= 768 && desserts.style.display === "flex")
+  ) {
+    buttonrefresh.style.display = "flex";
+  }
+  if (window.innerWidth > 768 || tea.style.display === "flex") {
+    buttonrefresh.style.display = "none";
+  }
+}
+setInterval(initRefresh, 1000);
+
+buttonrefresh.addEventListener("click", function () {
+  const elementscoffee = window.document.querySelectorAll(".nextcoffee");
+  const elementsdesserts = window.document.querySelectorAll(".nextdessert");
+  
+  buttonrefresh.style.display = "none";  
+  if (coffee.style.display === "flex") {
+    for (let i = 0; i < elementscoffee.length; i++) {
+      elementscoffee[i].style.display = "flex";
+    }
+  }
+  if (desserts.style.display === "flex") {
+    for (let i = 0; i < elementsdesserts.length; i++) {
+      elementsdesserts[i].style.display = "flex";
+    }
+  }
+});
+
+function backToSmallSize() {
+  if (window.innerWidth > 768) {
+    for (let i = 0; i < elementscoffee.length; i++) {
+      elementscoffee[i].style.display = "none";
+    }
+    for (let i = 0; i < elementsdesserts.length; i++) {
+      elementsdesserts[i].style.display = "none";
+    }
+    buttonrefresh.style.display = "none";
+  }
+}
+setInterval(backToSmallSize, 1000);
