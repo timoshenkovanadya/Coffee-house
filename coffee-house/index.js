03fad881b0443aae7b9f99c1892392a81a258ebb
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2023-12-11 22:36:33 +0300

    feat: update burger-menu

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,8 +1,6 @@
- const links = window.document.querySelectorAll(".nav-menu-item");
+const links = window.document.querySelectorAll(".nav-menu-item");
 
-
-
- window.document
+window.document
   .querySelector(".button-icon-burger")
   .addEventListener("click", function () {
     window.document
@@ -16,195 +14,112 @@
       .classList.toggle("burger-default-active");
     window.document
       .querySelector(".burger-default2")
-      .classList.toggle("burger-default-active");      
+      .classList.toggle("burger-default-active");
     window.document.querySelector(".new-menu").classList.toggle("new-menu2");
-    
   });
 
-   
- 
-  links.forEach(function(links) {
-    links.addEventListener("click", function () {
-      const menu = window.document.querySelector(".new-menu");
-       menu.classList.remove("new-menu2");
-       window.document
-       .querySelector(".burger-hover1")
-       .classList.remove("burger-hover-active");
-     window.document
-       .querySelector(".burger-hover2")
-       .classList.remove("burger-hover-active");
-     window.document
-       .querySelector(".burger-default1")
-       .classList.remove("burger-default-active");
-     window.document
-       .querySelector(".burger-default2")
-       .classList.remove("burger-default-active"); 
+links.forEach(function (links) {
+  links.addEventListener("click", function () {
+    const menu = window.document.querySelector(".new-menu");
+    menu.classList.remove("new-menu2");
+    window.document
+      .querySelector(".burger-hover1")
+      .classList.remove("burger-hover-active");
+    window.document
+      .querySelector(".burger-hover2")
+      .classList.remove("burger-hover-active");
+    window.document
+      .querySelector(".burger-default1")
+      .classList.remove("burger-default-active");
+    window.document
+      .querySelector(".burger-default2")
+      .classList.remove("burger-default-active");
+  });
+});
 
-      });
-    });
+function smallerSize() {
+  if (window.innerWidth > 768) {
+    document.querySelector(".new-menu").classList.remove("new-menu2");
+    window.document
+      .querySelector(".burger-hover1")
+      .classList.remove("burger-hover-active");
+    window.document
+      .querySelector(".burger-hover2")
+      .classList.remove("burger-hover-active");
+    window.document
+      .querySelector(".burger-default1")
+      .classList.remove("burger-default-active");
+    window.document
+      .querySelector(".burger-default2")
+      .classList.remove("burger-default-active");
+  }
+}
+window.addEventListener("resize", smallerSize);
 
-    function smallerSize() {
-      if (window.innerWidth > 768) {
-        document.querySelector(".new-menu").classList.remove('new-menu2');
-        window.document
-       .querySelector(".burger-hover1")
-       .classList.remove("burger-hover-active");
-     window.document
-       .querySelector(".burger-hover2")
-       .classList.remove("burger-hover-active");
-     window.document
-       .querySelector(".burger-default1")
-       .classList.remove("burger-default-active");
-     window.document
-       .querySelector(".burger-default2")
-       .classList.remove("burger-default-active"); 
-      }
-      
-    }    
-    window.addEventListener('resize', smallerSize);
-
- 
-
-  
 const BTN_LEFT = document.querySelector(".button-left");
 const BTN_RIGHT = document.querySelector(".button-right");
 const CAROUSEL = document.querySelector(".slider-line");
-const ITEM_LEFT = document.querySelector(".slide-2");
-const ITEM_RIGHT = document.querySelector(".slide-3");
 
-const createCardTemplate = () => {
-  const card = document.createElement("div");
-  card.classList.add("slide1-content");
-  return card;
-};
+let shift = 0;
 
-const moveLeft = () => {
-  CAROUSEL.classList.add("transition-left");
-  BTN_LEFT.removeEventListener("click", moveLeft);
-  BTN_RIGHT.removeEventListener("click", moveRight);
-};
-
-const moveRight = () => {
-  CAROUSEL.classList.add("transition-right");
-  BTN_LEFT.removeEventListener("click", moveLeft);
-  BTN_RIGHT.removeEventListener("click", moveRight);
-};
-
-BTN_LEFT.addEventListener("click", moveLeft);
-BTN_RIGHT.addEventListener("click", moveRight);
-
-CAROUSEL.addEventListener("animationend", (animationEvent) => {
-  let changedItem;
-  if (animationEvent.animationName === "move-left") {
-    CAROUSEL.classList.remove("transition-left");
-    changedItem = ITEM_LEFT;
-    document.querySelector(".slide-1").innerHTML = ITEM_LEFT.innerHTML;
-  } else {
-    CAROUSEL.classList.remove("transition-right");
-    changedItem = ITEM_RIGHT;
-    document.querySelector(".slide-1").innerHTML = ITEM_RIGHT.innerHTML;
+function moveLeft () {
+  shift += 1152;
+  if (shift > 2304) {
+    shift = 0;
   }
+  CAROUSEL.style.transform = `translateX(-${shift}px)`;
+}
 
-  changedItem.innerHTML = "";
-  for (let i = 0; i < 3; i++) {
-    const card = createCardTemplate();
-    card.innerText = Math.floor(Math.random() * 3);
-    changedItem.appendChild(card);
-  }
+let intervalId = setInterval(moveLeft, 5000); 
 
-  BTN_LEFT.addEventListener("click", moveLeft);
-  BTN_RIGHT.addEventListener("click", moveRight);
+BTN_RIGHT.addEventListener("click", function() {
+  moveLeft(); 
+  clearInterval(intervalId);
+  intervalId = setInterval(moveLeft, 5000);
 });
 
-// let slideIndex = 0;
-// const slides = window.document.querySelectorAll(".slide-1, .slide-2, .slide-3");
-// const lines = document.querySelectorAll(".line1, .line2, line3");
-// const buttonRight = document.querySelector(".button-right");
-// const buttonLeft = document.querySelector(".button-left");
-// let timerId;
-// let progressId;
-// let width = 0;
 
-// function showSlide(n) {
-//   slides.forEach((slide, index) => {
-//     slide.style.display = "none";
-//   });
 
-//   slides[n].style.display = "block";
-// }
+function moveRight () {
+  shift-= 1152;
+   if (shift<0) {
+    shift = 2304;}
+    CAROUSEL.style.transform = `translateX(-${shift}px)`;
+  
+}
+BTN_LEFT.addEventListener("click", moveRight);
 
-// function changeSlide() {
-//   showSlide(slideIndex);
-//   slideIndex++;
-//   if (slideIndex >= slides.length) {
-//     slideIndex = 0;
-//   }
-// }
+let touchStartX = 0;
+let touchEndX = 0;
 
-// function startTimer() {
-//   timerId = setInterval(changeSlide, 5000);
-// }
+function handleTouchStart(e) {
+  touchStartX = e.changedTouches[0].screenX;
+}
 
-// function stopTimer() {
-//   clearInterval(timerId);
-// }
+function handleTouchEnd(e) {
+  touchEndX = e.changedTouches[0].screenX;
+  handleSwipe();
+}
 
-// slides.forEach((slide) => {
-//   slide.addEventListener("mouseenter", stopTimer);
-//   slide.addEventListener("mouseleave", startTimer);
-//   slide.addEventListener("mousedown", stopTimer);
-//   slide.addEventListener("mouseup", startTimer);
-// });
+function handleSwipe() {
+  clearInterval(intervalId);
+  if (touchEndX < touchStartX) {
+    moveLeft();
+  } else {
+    moveRight();
+  }
+  intervalId = setInterval(moveLeft, 5000);
+}
 
-// buttonRight.addEventListener("click", function () {
-//   slideIndex++;
-//   if (slideIndex >= slides.length) {
-//     slideIndex = 0;
-//   }
-//   showSlide(slideIndex);
-// });
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchend', handleTouchEnd, false);
 
-// buttonLeft.addEventListener("click", function () {
-//   slideIndex--;
-//   if (slideIndex < 0) {
-//     slideIndex = slides.length - 1;
-//   }
-//   showSlide(slideIndex);
-// });
 
-// changeSlide();
-// startTimer();
-// let touchStartX = 0;
-// let touchEndX = 0;
 
-// function handleTouchStart(e) {
-//   touchStartX = e.changedTouches[0].screenX;
-// }
 
-// function handleTouchMove(e) {
-//   touchEndX = e.changedTouches[0].screenX;
-// }
 
-// function handleTouchEnd() {
-//   if (touchEndX < touchStartX) {
-//     slideIndex++;
-//     if (slideIndex >= slides.length) {
-//       slideIndex = 0;
-//     }
-//   }
 
-//   if (touchEndX > touchStartX) {
-//     slideIndex--;
-//     if (slideIndex < 0) {
-//       slideIndex = slides.length - 1;
-//     }
-//   }
 
-//   showSlide(slideIndex);
-// }
 
-// slides.forEach((slide) => {
-//   slide.addEventListener("touchstart", handleTouchStart);
-//   slide.addEventListener("touchmove", handleTouchMove);
-//   slide.addEventListener("touchend", handleTouchEnd);
-// });
+
+
